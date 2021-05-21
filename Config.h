49dad3223d96e42a613a16bd42dd9f8f04553f8c
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2021-05-21 13:33:24 +0430

    feat(IQWM): Systry

--- Config.h
+++ Config.h
@@ -3,23 +3,28 @@
 #define TERMCLASS "IQT-C"
 
 /* appearance */
-static unsigned int borderpx        = 1;        /* border pixel of windows */
-static unsigned int snap            = 32;       /* snap pixel */
-static unsigned int gappih          = 20;       /* horiz inner gap between windows */
-static unsigned int gappiv          = 10;       /* vert inner gap between windows */
-static unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
-static unsigned int gappov          = 30;       /* vert outer gap between windows and screen edge */
-static int swallowfloating          = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
-static int showbar                  = 1;        /* 0 means no bar */
-static int topbar                   = 1;        /* 0 means bottom bar */
-static char *fonts[]                = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#1C1E26";
-static char normbordercolor[]       = "#2E303E";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#FDF0ED";
-static char selbordercolor[]        = "#6C6F93";
-static char selbgcolor[]            = "#6C6F93";
+static unsigned int borderpx        	 = 1;        /* border pixel of windows */
+static unsigned int snap            	 = 32;       /* snap pixel */
+static unsigned int gappih          	 = 20;       /* horiz inner gap between windows */
+static unsigned int gappiv          	 = 10;       /* vert inner gap between windows */
+static unsigned int gappoh          	 = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov          	 = 30;       /* vert outer gap between windows and screen edge */
+static int swallowfloating          	 = 0;        /* 1 means swallow floating windows by default */
+static int smartgaps                	 = 0;        /* 1 means no outer gap when there is only one window */
+static int showbar                  	 = 1;        /* 0 means no bar */
+static int topbar                   	 = 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning = 0;   	 /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;   	 /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;  	     /* systray spacing */
+static const int systraypinningfailfirst = 1;   	 /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        	 = 1;        /* 0 means no systray */
+static char *fonts[]                	 = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char normbgcolor[]           	 = "#1C1E26";
+static char normbordercolor[]       	 = "#2E303E";
+static char normfgcolor[]           	 = "#bbbbbb";
+static char selfgcolor[]            	 = "#FDF0ED";
+static char selbordercolor[]        	 = "#6C6F93";
+static char selbgcolor[]            	 = "#6C6F93";
 static char *colors[][3] =
 {
     /*               fg           bg           border   */
